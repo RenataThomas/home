65b0f981dab0b7bafb81c5521b7f2850656cde84
Luiz Bento <32204385+luizhbento@users.noreply.github.com>
2025-12-04 15:00:57 -0300

    V2 Site Finalizado

--- src/data/GiftData.jsx
+++ src/data/GiftData.jsx
@@ -24,134 +24,134 @@ export const giftData = [
       'https://nubank.com.br/cobrar/dhv4ug/6930cb1f-b4b1-4a0d-ad7f-3708fcf4ddd5',
     linkCartao: 'https://buy.stripe.com/4gM5kDf5i68R2EwbKubZe0d',
   },
-  // {
-  //   titulo: 'Conjunto de Academia para a Noiva',
-  //   descricao: 'Pra noiva ir com estilo fazer suas atividades físicas.',
-  //   imagem: Presente02,
-  //   preco: 'R$ 98,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cbb1-d1fc-43e8-8fda-85d354b97173',
-  //   linkCartao: 'https://buy.stripe.com/3cIcN59KYdBj0wo7uebZe0e',
-  // },
-  // {
-  //   titulo: 'Kit “Cinema em Casa”',
-  //   descricao: 'Pipoca gourmet, refri e proibido dormir no filme.',
-  //   imagem: Presente03,
-  //   preco: 'R$ 129,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cbd2-41a6-4884-a4ed-7f466de99b18',
-  //   linkCartao: 'https://buy.stripe.com/fZu28r2iwgNv5QI15QbZe04',
-  // },
-  // {
-  //   titulo: 'Rodízio de Japonês para a Noiva',
-  //   descricao: 'Afinal, isso ajuda muito no bom humor da noiva.',
-  //   imagem: Presente04,
-  //   preco: 'R$ 199,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cbe6-8055-4c00-b41e-2688fedb9401',
-  //   linkCartao: 'https://buy.stripe.com/7sY5kDcXabtb3IAbKubZe0c',
-  // },
-  // {
-  //   titulo: 'Vale Massagem',
-  //   descricao: 'Quem será que vai usar primeiro?',
-  //   imagem: Presente05,
-  //   preco: 'R$ 199,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cbe6-8055-4c00-b41e-2688fedb9401',
-  //   linkCartao: 'https://buy.stripe.com/8x25kD0aodBja6Y5m6bZe05',
-  // },
-  // {
-  //   titulo: 'Cobertor para a Noiva que está sempre coberta de razão',
-  //   descricao: 'Fato!',
-  //   imagem: Presente06,
-  //   preco: 'R$ 249,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cc23-23d6-48c7-bd78-3cf4d4c60e1c',
-  //   linkCartao: 'https://buy.stripe.com/00w8wP8GU0Ox4ME3dYbZe0f',
-  // },
-  // {
-  //   titulo: 'Vale para a Noiva deixar o Noivo fazer Corujão no Sábado',
-  //   descricao: 'Sábado é aquele momento que junta a galera né.',
-  //   imagem: Presente07,
-  //   preco: 'R$ 249,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cc23-23d6-48c7-bd78-3cf4d4c60e1c',
-  //   linkCartao: 'https://buy.stripe.com/00wbJ14qE2WFcf63dYbZe0a',
-  // },
-  // {
-  //   titulo: 'Álbum em forma de livro',
-  //   descricao: 'Pra registrar fotos lindas e vergonhas compartilhadas.',
-  //   imagem: Presente08,
-  //   preco: 'R$ 299,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cc3a-a167-433e-a2f8-ae61397fe01f',
-  //   linkCartao: 'https://buy.stripe.com/9B68wP1es9l31AsaGqbZe03',
-  // },
-  // {
-  //   titulo: 'Kit “Manutenção da Paz Conjugal”',
-  //   descricao:
-  //     'Chocolate, earplugs e um aviso: “volto quando estiver mais calmo(a)”.',
-  //   imagem: Presente09,
-  //   preco: 'R$ 349,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cc4d-6400-45dc-9246-a6a4b340ab06',
-  //   linkCartao: 'https://buy.stripe.com/7sYcN52iw54Ngvm4i2bZe08',
-  // },
-  // {
-  //   titulo: 'Vale NETFLIX',
-  //   descricao: 'Pra noiva maratonar as séries todas de novo.',
-  //   imagem: Presente10,
-  //   preco: 'R$ 538,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cc61-2fa8-4a70-aee6-be2ed682aaec',
-  //   linkCartao: 'https://donate.stripe.com/bJebJ17CQ7cV0wocOybZe0g',
-  // },
-  // {
-  //   titulo: 'Ajuda na compra do Xiaomi Robot Vacuum X20',
-  //   descricao: 'Pra deixar a casa mais limpa e sobrar mais tempo pra nós.',
-  //   imagem: Presente11,
-  //   preco: 'R$ 580,00',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cc75-0739-4deb-bdf2-3862f3df4310',
-  //   linkCartao: 'https://buy.stripe.com/14A7sLg9m40J7YQ01MbZe07',
-  // },
-  // {
-  //   titulo: 'Faqueiro completo Inox',
-  //   descricao:
-  //     'Chega de almoçar com uma faca azul e um garfo preto ou vice-versa.',
-  //   imagem: Presente12,
-  //   preco: 'R$ 712,99',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cc86-fbff-4dc6-bc21-ad6b71af1403',
-  //   linkCartao: 'https://buy.stripe.com/cNieVd1es9l3gvm6qabZe09',
-  // },
-  // {
-  //   titulo: 'Jaqueta nova de moto para o Noivo',
-  //   descricao: 'Segurança + estilo = casamento feliz.',
-  //   imagem: Presente13,
-  //   preco: 'R$ 849,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cca5-0c32-4706-a6ef-8ea557523de4',
-  //   linkCartao: 'https://buy.stripe.com/eVq3cvcXa2WF7YQ4i2bZe06',
-  // },
-  // {
-  //   titulo: 'Upgrade no Setup Gamer do Noivo',
-  //   descricao:
-  //     'A cada ano fica mais difícil pro noivo rodar aquele Counter Strike liso.',
-  //   imagem: Presente14,
-  //   preco: 'R$ 1.019,90',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cccb-82cf-4862-9aa8-c0b971df6fd3',
-  //   linkCartao: 'https://buy.stripe.com/3cI8wPbT60OxfriaGqbZe0b',
-  // },
-  // {
-  //   titulo: 'Presente Customizado',
-  //   descricao:
-  //     'Pra você que não gostou de nenhuma opção ou quer mandar outro valor.',
-  //   imagem: Presente15,
-  //   preco: 'Valor Livre',
-  //   linkPix:
-  //     'https://nubank.com.br/cobrar/dhv4ug/6930cd0b-8652-4d56-a249-621b903d9dce',
-  //   linkCartao: 'https://buy.stripe.com/aFadR95uI2WF92U5m6bZe02',
-  // },
+  {
+    titulo: 'Conjunto de Academia para a Noiva',
+    descricao: 'Pra noiva ir com estilo fazer suas atividades físicas.',
+    imagem: Presente02,
+    preco: 'R$ 98,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cbb1-d1fc-43e8-8fda-85d354b97173',
+    linkCartao: 'https://buy.stripe.com/3cIcN59KYdBj0wo7uebZe0e',
+  },
+  {
+    titulo: 'Kit “Cinema em Casa”',
+    descricao: 'Pipoca gourmet, refri e proibido dormir no filme.',
+    imagem: Presente03,
+    preco: 'R$ 129,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cbd2-41a6-4884-a4ed-7f466de99b18',
+    linkCartao: 'https://buy.stripe.com/fZu28r2iwgNv5QI15QbZe04',
+  },
+  {
+    titulo: 'Rodízio de Japonês para a Noiva',
+    descricao: 'Afinal, isso ajuda muito no bom humor da noiva.',
+    imagem: Presente04,
+    preco: 'R$ 199,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cbe6-8055-4c00-b41e-2688fedb9401',
+    linkCartao: 'https://buy.stripe.com/7sY5kDcXabtb3IAbKubZe0c',
+  },
+  {
+    titulo: 'Vale Massagem',
+    descricao: 'Quem será que vai usar primeiro?',
+    imagem: Presente05,
+    preco: 'R$ 199,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cbe6-8055-4c00-b41e-2688fedb9401',
+    linkCartao: 'https://buy.stripe.com/8x25kD0aodBja6Y5m6bZe05',
+  },
+  {
+    titulo: 'Cobertor para a Noiva que está sempre coberta de razão',
+    descricao: 'Fato!',
+    imagem: Presente06,
+    preco: 'R$ 249,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cc23-23d6-48c7-bd78-3cf4d4c60e1c',
+    linkCartao: 'https://buy.stripe.com/00w8wP8GU0Ox4ME3dYbZe0f',
+  },
+  {
+    titulo: 'Vale para a Noiva deixar o Noivo fazer Corujão no Sábado',
+    descricao: 'Sábado é aquele momento que junta a galera né.',
+    imagem: Presente07,
+    preco: 'R$ 249,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cc23-23d6-48c7-bd78-3cf4d4c60e1c',
+    linkCartao: 'https://buy.stripe.com/00wbJ14qE2WFcf63dYbZe0a',
+  },
+  {
+    titulo: 'Álbum em forma de livro',
+    descricao: 'Pra registrar fotos lindas e vergonhas compartilhadas.',
+    imagem: Presente08,
+    preco: 'R$ 299,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cc3a-a167-433e-a2f8-ae61397fe01f',
+    linkCartao: 'https://buy.stripe.com/9B68wP1es9l31AsaGqbZe03',
+  },
+  {
+    titulo: 'Kit “Manutenção da Paz Conjugal”',
+    descricao:
+      'Chocolate, earplugs e um aviso: “volto quando estiver mais calmo(a)”.',
+    imagem: Presente09,
+    preco: 'R$ 349,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cc4d-6400-45dc-9246-a6a4b340ab06',
+    linkCartao: 'https://buy.stripe.com/7sYcN52iw54Ngvm4i2bZe08',
+  },
+  {
+    titulo: 'Vale NETFLIX',
+    descricao: 'Pra noiva maratonar as séries todas de novo.',
+    imagem: Presente10,
+    preco: 'R$ 538,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cc61-2fa8-4a70-aee6-be2ed682aaec',
+    linkCartao: 'https://donate.stripe.com/bJebJ17CQ7cV0wocOybZe0g',
+  },
+  {
+    titulo: 'Ajuda na compra do Xiaomi Robot Vacuum X20',
+    descricao: 'Pra deixar a casa mais limpa e sobrar mais tempo pra nós.',
+    imagem: Presente11,
+    preco: 'R$ 580,00',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cc75-0739-4deb-bdf2-3862f3df4310',
+    linkCartao: 'https://buy.stripe.com/14A7sLg9m40J7YQ01MbZe07',
+  },
+  {
+    titulo: 'Faqueiro completo Inox',
+    descricao:
+      'Chega de almoçar com uma faca azul e um garfo preto ou vice-versa.',
+    imagem: Presente12,
+    preco: 'R$ 712,99',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cc86-fbff-4dc6-bc21-ad6b71af1403',
+    linkCartao: 'https://buy.stripe.com/cNieVd1es9l3gvm6qabZe09',
+  },
+  {
+    titulo: 'Jaqueta nova de moto para o Noivo',
+    descricao: 'Segurança + estilo = casamento feliz.',
+    imagem: Presente13,
+    preco: 'R$ 849,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cca5-0c32-4706-a6ef-8ea557523de4',
+    linkCartao: 'https://buy.stripe.com/eVq3cvcXa2WF7YQ4i2bZe06',
+  },
+  {
+    titulo: 'Upgrade no Setup Gamer do Noivo',
+    descricao:
+      'A cada ano fica mais difícil pro noivo rodar aquele Counter Strike liso.',
+    imagem: Presente14,
+    preco: 'R$ 1.019,90',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cccb-82cf-4862-9aa8-c0b971df6fd3',
+    linkCartao: 'https://buy.stripe.com/3cI8wPbT60OxfriaGqbZe0b',
+  },
+  {
+    titulo: 'Presente Customizado',
+    descricao:
+      'Pra você que não gostou de nenhuma opção ou quer mandar outro valor.',
+    imagem: Presente15,
+    preco: 'Valor Livre',
+    linkPix:
+      'https://nubank.com.br/cobrar/dhv4ug/6930cd0b-8652-4d56-a249-621b903d9dce',
+    linkCartao: 'https://buy.stripe.com/aFadR95uI2WF92U5m6bZe02',
+  },
 ];
